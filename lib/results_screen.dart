274880ff12c30f3a1971fdf6bd06b7bc0dad71c4
Gastón Acosta <gastoneacosta@outlook.com>
2023-08-21 01:10:20 -0300

    minor fixes

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -9,7 +9,7 @@ class ResultsScreen extends StatelessWidget {
   final Function() switchScreen;
   final List<String> chosenAnswers;
 
-  List<Map<String, Object>> getSummaryData() {
+  List<Map<String, Object>> get summaryData {
     final List<Map<String, Object>> summary = [];
     for (var i = 0; i < chosenAnswers.length; i++) {
       summary.add({
@@ -24,7 +24,6 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final summaryData = getSummaryData();
     final numTotalQuestions = questions.length;
     final numCorrectQuestions = summaryData.where((data) {
       return data["correct_answer"] == data["user_answer"];
